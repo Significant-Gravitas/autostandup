5abe15e35bcc51223d011d0a749b1a5d09ed55fc
hunteraraujo <hunter_araujo@msn.com>
2023-10-12 13:32:55 -0700

    Add docstrings to Scheduler + TeamMember class

--- scheduler.py
+++ scheduler.py
@@ -5,11 +5,24 @@ from team_member import TeamMember
 import pytz
 
 class Scheduler:
+    """Scheduler class to manage timed jobs for sending status requests.
+    
+    Attributes:
+        scheduler: The APScheduler object.
+    """
+    
     def __init__(self) -> None:
+        """Initialize the Scheduler object and start the APScheduler."""
         self.scheduler: AsyncIOScheduler = AsyncIOScheduler()
         self.scheduler.start()
 
     def add_job(self, func: callable, member: TeamMember) -> None:
+        """Add a new job to the scheduler for a specific team member.
+        
+        Args:
+            func: The function to call when the job is run.
+            member: The TeamMember object for whom the job is added.
+        """
         time_zone = pytz.timezone(member.time_zone)
         trigger = CronTrigger(day_of_week='mon,wed,fri', hour=10, timezone=time_zone)
         job = self.scheduler.add_job(func, trigger, args=[member])
--- team_member.py
+++ team_member.py
@@ -1,5 +1,20 @@
 class TeamMember:
+    """TeamMember class to store individual team member details.
+    
+    Attributes:
+        discord_id: The Discord ID of the team member.
+        time_zone: The time zone in which the team member resides.
+        name: The name of the team member.
+    """
+    
     def __init__(self, discord_id: int, time_zone: str, name: str) -> None:
+        """Initialize a new TeamMember object.
+        
+        Args:
+            discord_id: The Discord ID of the team member.
+            time_zone: The time zone of the team member.
+            name: The name of the team member.
+        """
         self.discord_id: int = discord_id
         self.time_zone: str = time_zone
         self.name: str = name
