091b41789b71c3218e1c64afb24b1b2a5e7d07f0
hunteraraujo <hunter_araujo@msn.com>
2023-10-12 13:41:34 -0700

    Replace send_status_request print statement with database insert

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from discord import Intents
 from dotenv import load_dotenv
 from scheduler import Scheduler
 from team_member import TeamMember
+from status_db import StatusDB
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -19,10 +20,11 @@ CHANNEL_TOKEN = int(os.getenv('DISCORD_CHANNEL_TOKEN'))
 intents = Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+db = StatusDB()
 
 # Define a loop that runs every 48 hours to ping for status updates
 @tasks.loop(hours=48)
-async def ping_for_status():
+async def ping_for_status() -> None:
     # Get the guild (server) and channel using their IDs
     guild = bot.get_guild(GUILD_TOKEN)
     channel = guild.get_channel(CHANNEL_TOKEN)
@@ -33,11 +35,13 @@ async def send_status_request(member: TeamMember) -> None:
     user = bot.get_user(member.discord_id)
     if user:
         await user.send(f"Good morning {member.name}, time for your status update!")
-        # For now, let's just print whatever they reply with
         def check(m) -> bool:
             return m.author == user
         msg = await bot.wait_for('message', check=check)
-        print(f"Status update from {member.name}: {msg.content}")
+        
+        # Insert the status update into the database
+        db.insert_status(member.discord_id, member.name, msg.content)
+
 
 @bot.event
 async def on_ready() -> None:
