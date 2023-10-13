cd37c2d27a72b81619d75d23b40b233b10687d15
hunteraraujo <hunter_araujo@msn.com>
2023-10-12 23:12:22 -0700

    Refactor bot.py to use TeamMemberManager and WeeklyPostManager
    Refactor bot.py to use TeamMemberManager and WeeklyPostManager
    
    - Introduced TeamMemberManager to manage the loading and handling of team members from a JSON file.
    - Integrated WeeklyPostManager to manage the weekly status posts in Discord.
    - Moved logic for sending status requests and updating posts to their respective managers.
    - Added comments and documentation for improved readability and maintainability.
    
    This refactor aims to make the main bot script cleaner and more focused, by delegating specific tasks to specialized manager classes.

--- bot.py
+++ bot.py
@@ -1,12 +1,17 @@
 # Import required modules
 import os
 import json
+from typing import List
+import pytz
 from discord.ext import commands, tasks
 from discord import Intents
 from dotenv import load_dotenv
 from scheduler import Scheduler
 from team_member import TeamMember
 from status_db import StatusDB
+from datetime import datetime, timedelta
+from weekly_post_manager import WeeklyPostManager
+from team_member_manager import TeamMemberManager
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -22,6 +27,9 @@ intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 db = StatusDB()
 
+team_member_manager = None
+weekly_post_manager = None
+
 # Define a loop that runs every 48 hours to ping for status updates
 @tasks.loop(hours=48)
 async def ping_for_status() -> None:
@@ -35,31 +43,51 @@ async def send_status_request(member: TeamMember) -> None:
     user = bot.get_user(member.discord_id)
     if user:
         await user.send(f"Good morning {member.name}, time for your status update!")
+
         def check(m) -> bool:
             return m.author == user
+            
         msg = await bot.wait_for('message', check=check)
-        
+
         # Insert the status update into the database
         db.insert_status(member.discord_id, member.name, msg.content)
 
+        # Determine which weekday it is in the member's local time zone
+        utc_now = pytz.utc.localize(datetime.utcnow())
+        local_now = utc_now.astimezone(pytz.timezone(member.time_zone))
+        weekday = local_now.weekday()
+
+        # Update the Discord post using PostManager
+        await weekly_post_manager.update_post(member, weekday)
 
 @bot.event
 async def on_ready() -> None:
-    print("Bot is online!")
+    print("Bot is online!")  # Log that the bot is online
     
+    # Initialize a job scheduler for sending status requests to team members
     scheduler = Scheduler()
     
-    # Read team member data from JSON file
-    with open("team_members.json", "r") as f:
-        team_members_data = json.load(f)
+    team_member_manager = TeamMemberManager("team_members.json")
+    # Use TeamMemberManager to load team members from the JSON file
+    team_members = team_member_manager.load_team_members()
     
-    for member_data in team_members_data:
-        member = TeamMember(
-            discord_id=member_data["discord_id"], 
-            time_zone=member_data["time_zone"], 
-            name=member_data["name"]
-        )
+    # Get the Discord guild (server) and channel objects
+    guild = bot.get_guild(GUILD_TOKEN)
+    channel = guild.get_channel(CHANNEL_TOKEN)
+    
+    # Schedule status request jobs for each team member
+    for member in team_members:
         scheduler.add_job(send_status_request, member)
+
+    # Declare the global variable for the WeeklyPostManager
+    global weekly_post_manager
+    
+    # Initialize the WeeklyPostManager with the channel and team members
+    weekly_post_manager = WeeklyPostManager(channel, team_members)
+    
+    # Call the method to create the initial weekly post
+    await weekly_post_manager.initialize_post()
+
 
 # Run the bot
 bot.run(BOT_TOKEN)
