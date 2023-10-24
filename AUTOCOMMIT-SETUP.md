e42303688ea161162fb79094d1f2dda9bdaa1f1d
hunteraraujo <hunter_araujo@msn.com>
2023-10-24 14:38:54 -0700

    feat: Added AUTOCOMMIT-SETUP.md and autocommit.py
    - Created a setup guide for the `autocommit` script, including prerequisites, installation, configuration, and usage instructions.
    - Implemented the `autocommit.py` script, which utilizes the OpenAI API to generate commit messages based on staged changes in a Git repository.
    - The script provides options to automatically stage all changes, check if the current directory is a valid Git repository, and interactively generate and accept commit messages.

---
+++ AUTOCOMMIT-SETUP.md
@@ -0,0 +1,82 @@
+## Setting Up the `autocommit` Script
+
+`autocommit` is a command-line tool designed to assist developers in generating meaningful commit messages based on staged changes in a Git repository. By leveraging advanced language models, the tool provides contextually relevant commit messages, streamlining the development workflow and ensuring consistent commit conventions. Before using, ensure you've set up the tool with the appropriate OpenAI API key.
+
+### 1. **Prerequisites**:
+
+Ensure you have Python installed on your machine. The script will first look for `python3`, and if not found, will fallback to `python`.
+
+Install the OpenAI Python SDK. You can do this using pip:
+
+```bash
+pip install openai
+```
+
+### 2. **Installation**:
+
+1. Download the `autocommit` script from the provided link or location.
+2. Move the script to a directory in your PATH for easy access:
+
+```bash
+sudo mv /path/to/autocommit /usr/local/bin/autocommit
+```
+
+3. Make the script executable:
+
+```bash
+chmod +x /usr/local/bin/autocommit
+```
+
+### 3. **Configuration**:
+
+Before using the script, set up your OpenAI API key. 
+
+Open the script in a text editor:
+
+```bash
+nano /usr/local/bin/autocommit
+```
+
+Find the line:
+
+```python
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+```
+
+Replace `YOUR_OPENAI_API_KEY` with your actual API key:
+
+```python
+openai.api_key = 'YOUR_ACTUAL_OPENAI_API_KEY'
+```
+
+Save and close the file.
+
+### 4. **Usage**:
+
+Before generating a commit message, ensure your changes are staged. If you're unfamiliar with staging, it's the process of marking changes for inclusion in the next commit. Use:
+
+```bash
+git add <filename>
+```
+
+Alternatively, the `autocommit` script provides a `--stage-all` flag to automatically stage all changes for you:
+
+```bash
+autocommit --stage-all
+```
+
+Once staged, run the script:
+
+```bash
+autocommit
+```
+
+The script will suggest a commit message based on the staged changes. Choose to accept the suggested message or generate a new one.
+
+### **Note**:
+
+Always review the auto-generated commit messages before pushing. Ensure they accurately capture the essence of your changes. If needed, amend the last commit with a manually crafted message:
+
+```bash
+git commit --amend
+```
---
+++ autocommit.py
@@ -0,0 +1,94 @@
+#!/bin/sh
+''':'
+for interpreter in python3 python; do
+    if command -v $interpreter > /dev/null 2>&1; then
+        exec "$interpreter" "$0" "$@"
+        exit
+    fi
+done
+echo "No python interpreter found."
+exit 1
+' '''
+
+import os
+import openai
+import sys
+
+# Initialize the OpenAI API
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+def get_staged_diff():
+    """Get the git diff of the staged changes in the current repository."""
+    return os.popen('git diff --cached').read()
+
+def stage_all_changes():
+    """Stage all changes in the current repository."""
+    os.system('git add .')
+
+def generate_commit_message(diff):
+    """Generate a commit title and message using the OpenAI API."""
+    
+    system_message = {
+        "role": "system",
+        "content": (
+            "You are an expert commit message generator. You should generate a single commit message from a git diff in the following format:"
+            "\n\n<type>: <description>\n\n[bulleted body]\n\n"
+            "Acceptable types for your commit message are:"
+            "\n- build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)"
+            "\n- ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)"
+            "\n- docs: Documentation only changes"
+            "\n- feat: A new feature"
+            "\n- fix: A bug fix"
+            "\n- perf: A code change that improves performance"
+            "\n- refactor: A code change that neither fixes a bug nor adds a feature"
+            "\n- style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
+            "\n- test: Adding missing tests or correcting existing tests"
+        )
+    }
+
+    user_message = {
+        "role": "user",
+        "content": diff
+    }
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k-0613",
+        messages=[system_message, user_message]
+    )
+
+    return response.choices[0].message['content'].strip()
+
+def is_valid_git_repo():
+    """Check if the current directory is a valid Git repository."""
+    exit_status = os.system('git rev-parse --is-inside-work-tree > /dev/null 2>&1')
+    return exit_status == 0
+
+def main():
+    if "--stage-all" in sys.argv:
+        stage_all_changes()
+        
+    if not is_valid_git_repo():
+        print("Error: This is not a valid Git repository.")
+        return
+
+    diff = get_staged_diff()
+    if not diff:
+        print("No staged changes detected. Ensure you've staged your changes using 'git add <filename>' or use the '--stage-all' flag to stage all changes.")
+        return
+
+    while True:
+        commit_msg = generate_commit_message(diff)
+        print(f"Suggested Commit Message:\n\n{commit_msg}")
+        choice = input("\nDo you want to [a]ccept, or [g]enerate a new one? ").lower()
+
+        if choice == 'a':
+            os.system(f'git commit -m "{commit_msg}"')
+            print("Changes committed!")
+            break
+        elif choice == 'g':
+            print("Generating a new commit message...\n")
+        else:
+            print("Invalid choice. Please choose [a]ccept or [g]enerate.")
+
+if __name__ == '__main__':
+    main()
